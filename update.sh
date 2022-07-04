654efffe24ff29ad0e35ebb188a697b384b72e14
Ryan Horne <rjhorne@DESKTOP-9I59M85.localdomain>
2022-07-04 12:12:26 -0600

    initial commit

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+if !which wget >/dev/null 2>&1; then >&2 echo "please install wget"; exit 1; fi
+
+LISTS="
+https://raw.githubusercontent.com/brave/goggles-quickstart/main/goggles/no_pinterest.goggle
+https://raw.githubusercontent.com/brave/goggles-quickstart/main/goggles/copycats_removal.goggle"
+
+SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
+cd "$SCRIPTDIR" || exit 1
+
+rm "$SCRIPTDIR"/*.goggle.*
+cat "$SCRIPTDIR"/rm_bad_base.txt > "$SCRIPTDIR"/rm_bad.goggle
+
+for LIST in $LISTS; do
+	wget $LIST
+	egrep -v -E '^!|^$' "$SCRIPTDIR"/$(basename "$LIST") >> "$SCRIPTDIR"/rm_bad.goggle
+done
+
+cd - >/dev/null || exit 1
