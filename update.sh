064940fd1cab0e67a912532c901667cdc6a0109d
Ryan Horne <rjhorne@DESKTOP-9I59M85.localdomain>
2022-07-04 14:01:21 -0600

    changed to submodules

--- update.sh
+++ update.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env sh
 
-if !which wget >/dev/null 2>&1; then >&2 echo "please install wget"; exit 1; fi
+ALWAYS_LISTS="
+brave-goggles-quickstart/goggles/no_pinterest.goggle
+brave-goggles-quickstart/goggles/copycats_removal.goggle"
 
-LISTS="
-https://raw.githubusercontent.com/brave/goggles-quickstart/main/goggles/no_pinterest.goggle
-https://raw.githubusercontent.com/brave/goggles-quickstart/main/goggles/copycats_removal.goggle"
-
-SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(dirname "$-1")
 SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
 cd "$SCRIPTDIR" || exit 1
 
 rm "$SCRIPTDIR"/*.goggle*
-cat "$SCRIPTDIR"/rm_bad_base.txt > "$SCRIPTDIR"/rm_bad.goggle
 
-for LIST in $LISTS; do
-	wget $LIST
-	egrep -v -E '^!|^$' "$SCRIPTDIR"/$(basename "$LIST") >> "$SCRIPTDIR"/rm_bad.goggle
+# rm_bad
+cat "$SCRIPTDIR"/rm_bad_base.txt > "$SCRIPTDIR"/rm_bad.goggle
+for LIST in $ALWAYS_LISTS; do
+	egrep -v -E '^!|^$' "$SCRIPTDIR"/"$LIST" >> "$SCRIPTDIR"/rm_bad.goggle
+done
+
+# prog_sources
+cat "$SCRIPTDIR"/prog_sources_base.txt > "$SCRIPTDIR"/prog_sources.goggle
+for LIST in $ALWAYS_LISTS; do
+	egrep -v -E '^!|^$' "$SCRIPTDIR"/"$LIST" >> "$SCRIPTDIR"/prog_sources.goggle
 done
 
 cd - >/dev/null || exit 1
-
-echo "hard coded URL:" 'https%3A%2F%2Fraw.githubusercontent.com%2Fcd-ryan%2Fgoggles%2Fmain%2Frm_bad.goggle'
+echo "hard coded URLs:"
+echo 'https://raw.githubusercontent.com/cd-ryan/goggles/main/rm_bad.goggle'
+echo 'https://raw.githubusercontent.com/cd-ryan/goggles/main/prog_sources.goggle'
