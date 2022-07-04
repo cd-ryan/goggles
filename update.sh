549518aaf4ce36b92a0503ddaf9eeb754ce62a4a
Ryan Horne <rjhorne@DESKTOP-9I59M85.localdomain>
2022-07-04 14:30:32 -0600

    now that understand the syntax... here is another attempt

--- update.sh
+++ update.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env sh
 
+SCRIPTDIR=$(dirname "$-1")
+SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
+
 ALWAYS_LISTS="
 brave-goggles-quickstart/goggles/no_pinterest.goggle
 brave-goggles-quickstart/goggles/copycats_removal.goggle"
 
-SCRIPTDIR=$(dirname "$-1")
-SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
-cd "$SCRIPTDIR" || exit 1
+MY_GOGGLES="
+rm_bad.goggle
+prog_sources.goggle"
 
-rm "$SCRIPTDIR"/*.goggle*
-
-# rm_bad
-cat "$SCRIPTDIR"/rm_bad_base.txt > "$SCRIPTDIR"/rm_bad.goggle
-for LIST in $ALWAYS_LISTS; do
-	egrep -v -E '^!|^$' "$SCRIPTDIR"/"$LIST" >> "$SCRIPTDIR"/rm_bad.goggle
+for GOGGLE in $MY_GOGGLES; do
+	cat "$SCRIPTDIR"/"${GOGGLE%.*}_base.txt" > "$SCRIPTDIR"/"$GOGGLE"
+	for LIST in $ALWAYS_LISTS; do
+		echo "! from subrepo path: $LIST" >> "$SCRIPTDIR"/"$GOGGLE"
+		egrep -v -E '^!|^$' "$SCRIPTDIR"/"$LIST" >> "$SCRIPTDIR"/"$GOGGLE"
+		echo "" >> "$SCRIPTDIR"/"$GOGGLE"
+	done
 done
 
-# prog_sources
-cat "$SCRIPTDIR"/prog_sources_base.txt > "$SCRIPTDIR"/prog_sources.goggle
-for LIST in $ALWAYS_LISTS; do
-	egrep -v -E '^!|^$' "$SCRIPTDIR"/"$LIST" >> "$SCRIPTDIR"/prog_sources.goggle
+BASEURL="https://raw.githubusercontent.com/cd-ryan/goggles/main"
+echo "raw URLs:"
+for GOGGLE in $MY_GOGGLES; do
+	echo "$BASEURL/$GOGGLE"
 done
 
-cd - >/dev/null || exit 1
-echo "hard coded URLs:"
-echo 'https://raw.githubusercontent.com/cd-ryan/goggles/main/rm_bad.goggle'
-echo 'https://raw.githubusercontent.com/cd-ryan/goggles/main/prog_sources.goggle'
+echo; echo "create link: https://search.brave.com/goggles/create"
+
